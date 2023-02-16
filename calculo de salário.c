509233cab2ba917633f744b1d5f07eb49c0f5370
Vinícius Cabral de Souza <102524171+ViniCabralS@users.noreply.github.com>
2023-02-16 09:52:26 -0300

    Create calculo de salário.c

---
+++ calculo de salário.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main() {
+    int cod, horas;
+    float salary_total, salario;
+ 
+    scanf("%d", cod);
+    scanf("%d", horas);
+    scanf("%d", salario);
+ 
+    salary_total = salario*horas;
+ 
+    printf("NUMBER = %d\n", cod);
+    printf("SALARY = U$ %.2f\n", salary_total);
+ 
+ 
+    return 0;
+}
