88c620321fdadc718051c8f00ec8522007c7c767
Vinícius Cabral de Souza <102524171+ViniCabralS@users.noreply.github.com>
2023-02-16 09:56:18 -0300

    Create soma_simples.c

---
+++ soma_simples.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    int a, b, SOMA;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    
+    SOMA = a+b;
+    printf("SOMA = %d\n", SOMA);
+    return 0;
+    
+}
